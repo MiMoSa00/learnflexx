ff94986f435166034b0b8edebc6397eb674894ee
Oluwamomi <oluwamomiokpaise@gmail.com>
2026-03-29 12:50:39 +0100

    chore: prepare for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+} satisfies NextConfig
 
 export default nextConfig
